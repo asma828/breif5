9614f18c035bf6f9010d2539cb772f3e16e76798
youssef elhoubi <elhoubiyoussef@gmail.com>
2024-11-12 10:44:27 +0100

    the sider is complated

---
+++ js/catalogue.js
@@ -0,0 +1,16 @@
+const sliderContainer = document.querySelector("#sliderContainer");
+const leftArrow = document.querySelector("#leftArrow");
+const rightArrow = document.querySelector("#rightArrow");
+
+
+sliderContainer.addEventListener("wheel", (e) => {
+    e.preventDefault();
+});
+rightArrow.addEventListener("click",()=>{
+    sliderContainer.style.scrollBehavior = "smooth"
+    sliderContainer.scrollLeft += sliderContainer.offsetWidth  * 1.005
+})
+leftArrow.addEventListener("click",()=>{
+    sliderContainer.style.scrollBehavior = "smooth"
+    sliderContainer.scrollLeft -= sliderContainer.offsetWidth  * 1
+})
