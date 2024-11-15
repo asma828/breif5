b23c23cfff512063b692f1029c0df8fdc498ff6d
asma828 <asmalachhab66@gmail.com>
2024-11-15 19:29:00 +0100

    addproduct to cart function

--- js/panier.js
+++ js/panier.js
@@ -5,3 +5,22 @@ document.getElementById('cartIcon').addEventListener('click', function() {
 document.getElementById('closeCart').addEventListener('click', function() {
     document.getElementById('cart').classList.add('translate-x-full');
 });
+
+
+
+function addToCart(name, price, imageSrc) {
+    const existingCartItem = cart.find(item => item.name === name);
+
+    if (existingCartItem) {
+        existingCartItem.quantity += 1; 
+    } else {
+        cart.push({
+            name: name,
+            price: parseFloat(price),
+            basePrice: parseFloat(price), 
+            imageSrc: imageSrc,
+            size: 'M', 
+            quantity: 1
+        });
+    }
+}
