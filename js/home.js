f4cbeb3042275397c2c22f141a57f2cb1fb88232
hamzalhadouchi <hamzabniss16@gmail.com>
2024-11-16 16:16:57 +0100

    Fetch product data and display in the catalog

--- js/home.js
+++ js/home.js
@@ -36,3 +36,56 @@ buttons.forEach(button => {
         startAutoSlide(); 
     });
 });
+
+const url = "https://gist.githubusercontent.com/EssadeqBillouche/72ca6ff79f3f364c962fb11de46982ee/raw/product.json";
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+
+        const products = data.products;
+        
+        for (let i = 0; i < products.length; i++) {
+            const product = products[i];
+            const cards = document.getElementById('cards');
+            const card = document.createElement('div');
+            card.classList.add('bg-white', 'min-w-[265px]', 'w-[265px]', 'flex', 'flex-col', 'justify-around', 'items-center', 'h-[350px]', 'm-3', 'rounded-lg');
+            
+            card.innerHTML = `
+            <div>
+                <div class="bg-white min-w-[240px] w-[265px] flex flex-col justify-between items-center h-[320px] m-3 rounded-lg p-4 bg-gray-200">
+                    <div class="w-28 h-60">
+                        <img class="object-cover" src="${product.img}" alt="">
+                    </div>
+                    <div class="flex justify-around flex-col h-20">
+                        <div class="h-12">
+                            <h1 class="font-bold">${product.name.slice(0, 20)}...</h1>
+                        </div>
+                        <div class="w-60 flex justify-between">
+                            <div class="text-orange-400">
+                                <span class="font-bold"> ${product.price}</span>
+                                <span class="font-bold">Dh</span>
+                            </div>
+                            <button>
+                                <div class="w-32 h-8 bg-white rounded-lg text-orange-400 flex justify-around items-center text-sm border-solid border-orange-400 hover:text-white hover:border-orange-400 border-4 hover:border-none hover:bg-orange-400">
+                                    <i class="fa-solid fa-cart-shopping"></i>
+                                    <p class="font-bold">Add to cart</p>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+            cards.appendChild(card);
+        }
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
