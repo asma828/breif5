0081e6abeb6f8a0f049f0beaab304ad9f9118697
youssef elhoubi <elhoubiyoussef@gmail.com>
2024-11-16 17:47:02 +0100

    added the berg menu with the help of Essadeq

--- js/catalogue.js
+++ js/catalogue.js
@@ -523,3 +523,25 @@ pagebuttons.forEach(element => {
         }
     })
 });
+
+// burger menu token from Essadeq 
+const burger_button = document.getElementById("burger_button");
+const menu_burger = document.getElementById("menu_burger");
+const close_button = document.getElementById("button_to_close");
+
+burger_button.addEventListener("click", () => {
+    console.log("burger is clicked ");
+    
+    menu_burger.classList.add("open");
+});
+
+
+close_button.addEventListener("click", () => {
+    menu_burger.classList.remove("open");
+});
+
+menu_burger.addEventListener("click", (event) => {
+    if (event.target === menu_burger) {
+        menu_burger.classList.remove("open");
+    }
+});
