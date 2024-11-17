1a3558ddc4ba3458bc4d1e8b8860600d2061d59e
taouamwalideddine <wtaouam@gmail.com>
2024-11-17 12:03:31 +0100

    fixed burger menu

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,22 @@
+const burger_button = document.getElementById("burger_button");
+const menu_burger = document.getElementById("menu_burger");
+const close_button = document.getElementById("button_to_close");
+
+burger_button.addEventListener("click", () => {
+    menu_burger.classList.add("open");
+});
+
+  
+close_button.addEventListener("click", () => {
+    menu_burger.classList.remove("open");
+});
+
+menu_burger.addEventListener("click", (event) => {
+    if (event.target === menu_burger) {
+        menu_burger.classList.remove("open");
+    }
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const refillBtn = document.getElementById("refillBtn");
     const submitBtn = document.getElementById("submitBtn");
