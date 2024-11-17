7621d61955867aff80c7591ae336d32a8bd9eb6c
hamzalhadouchi <hamzabniss16@gmail.com>
2024-11-17 15:50:33 +0100

    modifer les imag pour le headre

--- js/home.js
+++ js/home.js
@@ -9,6 +9,60 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 const intervalTime = 5000; 
 let autoSlideInterval;
 
+const url = "https://gist.githubusercontent.com/EssadeqBillouche/72ca6ff79f3f364c962fb11de46982ee/raw/product.json";
+
+fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+
+        const products = data.products;
+        
+        for (let i = 0; i < products.length; i++) {
+            const product = products[i];
+            
+            const cards = document.getElementById('cards');
+            const card = document.createElement('div');
+            card.classList.add('bg-white', 'min-w-[265px]', 'w-[265px]', 'flex', 'flex-col', 'justify-around', 'items-center', 'h-[350px]', 'm-3', 'rounded-lg');
+            
+            card.innerHTML = `
+            <div>
+        <div class="bg-white min-w-[240px] w-[265px] flex flex-col justify-between items-center h-[320px] m-3 rounded-lg p-4 bg-gray-200">
+            <div class="w-28 h-60">
+                <img class="object-cover" src="${product.img}" alt="">
+            </div>
+            <div class="flex justify-around flex-col h-20">
+                <div class="h-12">
+                    <h1 class="font-bold">${product.name.slice(0, 20)}...</h1>
+                </div>
+                <div class="w-60 flex justify-between">
+                    <div class="text-orange-400">
+                        <span class="font-bold"> ${product.price}</span>
+                        <span class="font-bold">Dh</span>
+                    </div>
+                    <button>
+                        <div class="w-32 h-8 bg-white rounded-lg text-orange-400 flex justify-around items-center text-sm border-solid border-orange-400 hover:text-white hover:border-orange-400 border-4 hover:border-none hover:bg-orange-400">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            <p class="font-bold">Add to cart</p>
+                        </div>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+            `;
+            cards.appendChild(card);
+        }
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
+
 function moveToNextSlide(offset) {
     const slides = document.querySelector("[data-slides]");
     const activeSlide = slides.querySelector("[data-active]");
@@ -37,62 +91,12 @@ buttons.forEach(button => {
     });
 });
 
-const url = "https://gist.githubusercontent.com/EssadeqBillouche/72ca6ff79f3f364c962fb11de46982ee/raw/product.json";
-
-fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log(data)
-
-        const products = data.products;
-        
-        for (let i = 0; i < products.length; i++) {
-            const product = products[i];
-            const cards = document.getElementById('cards');
-            const card = document.createElement('div');
-            card.classList.add('bg-white', 'min-w-[265px]', 'w-[265px]', 'flex', 'flex-col', 'justify-around', 'items-center', 'h-[350px]', 'm-3', 'rounded-lg');
-            
-            card.innerHTML = `
-            <div>
-                <div class="bg-white min-w-[240px] w-[265px] flex flex-col justify-between items-center h-[320px] m-3 rounded-lg p-4 bg-gray-200">
-                    <div class="w-28 h-60">
-                        <img class="object-cover" src="${product.img}" alt="">
-                    </div>
-                    <div class="flex justify-around flex-col h-20">
-                        <div class="h-12">
-                            <h1 class="font-bold">${product.name.slice(0, 20)}...</h1>
-                        </div>
-                        <div class="w-60 flex justify-between">
-                            <div class="text-orange-400">
-                                <span class="font-bold"> ${product.price}</span>
-                                <span class="font-bold">Dh</span>
-                            </div>
-                            <button>
-                                <div class="w-32 h-8 bg-white rounded-lg text-orange-400 flex justify-around items-center text-sm border-solid border-orange-400 hover:text-white hover:border-orange-400 border-4 hover:border-none hover:bg-orange-400">
-                                    <i class="fa-solid fa-cart-shopping"></i>
-                                    <p class="font-bold">Add to cart</p>
-                                </div>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            `;
-            cards.appendChild(card);
-        }
-    })
-    .catch(error => {
-        console.error("Error fetching data:", error);
-    });
-
-    let scroll = document.querySelector("#scroll");
+let scroll = document.querySelector("#scroll");
 let backBtn = document.getElementById("backbtn");
 let nextBtn = document.getElementById("nextbtn");
+
+
+
 
 backBtn.addEventListener("click", () => {
     scroll.style.scrollBehavior = "smooth";
